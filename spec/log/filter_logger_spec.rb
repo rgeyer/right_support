51fbdb5842fd2c02a611d6c95eba9eec5ec88c8e
Tony Spataro <tony@rightscale.com>
2012-01-25 12:12:02 -0800

    Add a few more pending specs for FilterLogger

--- spec/log/filter_logger_spec.rb
+++ spec/log/filter_logger_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe RightSupport::Log::FilterLogger do
-  it 'has test coverage'
+  it 'does method_missing correctly'
+  it 'does respond_to? correctly'
+  it 'transparently proxies Logger methods to underlying object'
+  it 'filters log messages'
 end
