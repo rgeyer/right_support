648264e9d14f3fde6e3be4ace485abd886b210df
Sergey Sergyenko <sergey.sergyenko@gmail.com>
2012-02-01 10:24:01 -0800

    Added SSLPatch as separate module

--- lib/right_support/net/ssl.rb
+++ lib/right_support/net/ssl.rb
@@ -11,73 +11,94 @@
 module RightSupport::Net
   # A helper module that provides monkey patch for OpenSSL::SSL.verify_certificate_identity method.
   #
-  # To start use it, all you need is to register DNS name of the host, which you will trust for sure.
-  # This module automatically adds OpenSSL library and reopens verify_certificate_identity method, to
-  # change the SSL algorythm of hostname verification.
-  module DNSResolver
-    class << self
-      @@registered_hostnames_hash = {}
+  # To start use it, all you need is to enable it and then register DNS name of the host, which 
+  # you will trust for sure. This module automatically adds OpenSSL library and reopens 
+  # verify_certificate_identity method, to change the SSL algorythm of hostname verification.
+  module SSLPatch
+  class << self
+    @@status = false
 
-      def get_trusted_dns_name(ip)
-        @@registered_hostnames_hash[ip]
-      end
+    def enable!
+      return if @@status
+      @@status = true
 
-      def register_trusted_dns_name(hostname)
-        trust_dns_completely
-        begin
-          infos = Socket.getaddrinfo(hostname, 443, Socket::AF_INET, Socket::SOCK_STREAM, Socket::IPPROTO_TCP)
-        rescue Exception => e
-          Log.error "Rescued #{e.class.name} resolving Repose hostnames: #{e.message}; retrying"
-          retry
+      require 'openssl'
+      OpenSSL::SSL.module_exec do
+        def verify_certificate_identity(cert, hostname)
+          actual_hostname = DNSResolver.get_trusted_dns_name(hostname)
+          verify_certificate_identity_without_hack(cert, actual_hostname ? actual_hostname : hostname)
         end
+        module_function :verify_certificate_identity
 
-        #Randomly permute the addrinfos of each hostname to help spread load.
-        infos.shuffle.each do |info|
-          ip = info[3]
-          @@registered_hostnames_hash[ip] = hostname
-        end
-      end
-
-      private
-
-      def trust_dns_completely
-        require 'openssl'
-        OpenSSL::SSL.module_exec do
-          def verify_certificate_identity(cert, hostname)
-            actual_hostname = RightSupport::Net::DNSResolver.get_trusted_dns_name(hostname)
-            verify_certificate_identity_without_hack(cert, actual_hostname ? actual_hostname : hostname)
-          end
-          module_function :verify_certificate_identity
-
-          # The original module function of OpenSSL::SSL
-          def verify_certificate_identity_without_hack(cert, hostname)
-            should_verify_common_name = true
-            cert.extensions.each{|ext|
-              next if ext.oid != "subjectAltName"
-              ext.value.split(/,\s+/).each{|general_name|
-                if /\ADNS:(.*)/ =~ general_name
-                  should_verify_common_name = false
-                  reg = Regexp.escape($1).gsub(/\\\*/, "[^.]+")
-                  return true if /\A#{reg}\z/i =~ hostname
-                elsif /\AIP Address:(.*)/ =~ general_name
-                  should_verify_common_name = false
-                  return true if $1 == hostname
-                end
-              }
+        # The original module function of OpenSSL::SSL
+        def verify_certificate_identity_without_hack(cert, hostname)
+          should_verify_common_name = true
+          cert.extensions.each{|ext|
+            next if ext.oid != "subjectAltName"
+            ext.value.split(/,\s+/).each{|general_name|
+              if /\ADNS:(.*)/ =~ general_name
+                should_verify_common_name = false
+                reg = Regexp.escape($1).gsub(/\\\*/, "[^.]+")
+                return true if /\A#{reg}\z/i =~ hostname
+              elsif /\AIP Address:(.*)/ =~ general_name
+                should_verify_common_name = false
+                return true if $1 == hostname
+              end
             }
-            if should_verify_common_name
-              cert.subject.to_a.each{|oid, value|
-                if oid == "CN"
-                  reg = Regexp.escape(value).gsub(/\\\*/, "[^.]+")
-                  return true if /\A#{reg}\z/i =~ hostname
-                end
-              }
-            end
-            return false
+          }
+          if should_verify_common_name
+            cert.subject.to_a.each{|oid, value|
+              if oid == "CN"
+                reg = Regexp.escape(value).gsub(/\\\*/, "[^.]+")
+                return true if /\A#{reg}\z/i =~ hostname
+              end
+            }
           end
-          module_function :verify_certificate_identity_without_hack
+          return false
         end
+        module_function :verify_certificate_identity_without_hack
       end
     end
+
+    def disable!
+      @@status = false
+    end
+
+    def enabled?
+      @@status
+    end
+
   end
+end
+
+module DNSResolver
+  class << self
+    @@registered_hostnames_hash = {}
+
+    def get_trusted_dns_name(ip)
+      @@registered_hostnames_hash[ip] if SSLPatch.enabled?
+    end
+
+    def register_trusted_dns_name(hostname)
+      unless SSLPatch.enabled?
+        puts "You have to enbale SSL monkey patch to use it!"
+        return
+      end
+      begin
+        infos = Socket.getaddrinfo(hostname, 443, Socket::AF_INET, Socket::SOCK_STREAM, Socket::IPPROTO_TCP)
+      rescue Exception => e
+        Log.error "Rescued #{e.class.name} resolving Repose hostnames: #{e.message}; retrying"
+        retry
+      end
+
+      #Randomly permute the addrinfos of each hostname to help spread load.
+      infos.shuffle.each do |info|
+        ip = info[3]
+        @@registered_hostnames_hash[ip] = hostname
+      end
+    end
+
+  end
+end
+
 end
